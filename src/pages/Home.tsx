17912348b663900a254868df99c0e7779ad0bc54
Sourav Basak <devsourav030@gmail.com>
2026-06-11 13:23:40 +0530

    deployment fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import { ArrowRight } from 'lucide-react';
 import { products } from '../data/products';
 import ProductCard from '../components/ProductCard';
 
